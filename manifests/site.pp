703d34d5320c5a57e5b8e7f94a33c2b46daa0dc3
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-19 13:43:24 +0200

    Add changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,12 @@ hiera_include('classes')
 
 #$myvhosts = hiera('apache::vhosts', {})
 #create_resources('apache::vhost', $myvhosts)
+$mserver = hiera('mysql::server', {})
+create_resources('mysql::server', $mserver)
 
 
-$mod = hiera('apache::mod', {})
-create_resources('apache::mod', $mod)
+#$mod = hiera('apache::mod', {})
+#create_resources('apache::mod', $mod)
 
 
 $node_users = hiera_hash('users')
